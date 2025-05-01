f8945d7c02d6d0b8405209a16ede0296c72ef5a4
LucasPetruci <lucaspetruci100@gmail.com>
2025-05-01 15:51:08 -0300

    feat: implement LocationController

---
+++ server/src/Controllers/LocationController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Http\HttpStatus;
+use App\Http\Response\ResponseBuilder;
+use App\Services\LocationService;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class LocationController
+{
+    public function __construct(private LocationService $service) {}
+
+    public function list(Request $request, Response $response): Response
+    {
+        $params = $request->getQueryParams();
+
+        $locations = isset($params['condominium_id'])
+            ? $this->service->listByCondominium($params['condominium_id'])
+            : $this->service->list();
+
+        $data = ['locations' => $locations];
+
+        $response = ResponseBuilder::respondWithData($response, data: $data);
+
+        return $response;
+    }
+
+    public function find(Request $request, Response $response, array $args): Response
+    {
+        $data = ['location' => $this->service->find($args['id'])];
+
+        $response = ResponseBuilder::respondWithData($response, data: $data);
+
+        return $response;
+    }
+
+    public function create(Request $request, Response $response): Response
+    {
+        $body = $request->getParsedBody();
+
+        
+        $data = ['location' => $this->service->create($body)];
+
+        $response = ResponseBuilder::respondWithData($response, HttpStatus::Created, $data);
+
+        return $response;
+    }
+
+    public function update(Request $request, Response $response, array $args): Response
+    {
+        $body = $request->getParsedBody();
+
+        $data = ['location' => $this->service->update($args['id'], $body)];
+
+
+        $response = ResponseBuilder::respondWithData($response, data: $data);
+
+        return $response;
+    }
+
+    public function delete(Request $request, Response $response, array $args): Response
+    {
+        $deleted = $this->service->delete($args['id']);
+
+        $status = $deleted ? HttpStatus::OK : HttpStatus::ServerError;
+
+        $response = ResponseBuilder::respondWithData($response, $status);
+
+        return $response;
+    }
+}
